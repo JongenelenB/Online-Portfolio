e2c0c354f24fec195acc25316cc1e24720fc8437
JongenelenB <bjorn.jongenelen11@gmail.com>
2026-01-25 15:17:51 +0100

    adding social icons and links to profile

--- Online-portolio/src/components/Header.tsx
+++ Online-portolio/src/components/Header.tsx
@@ -1,4 +1,21 @@
 export default function Header() {
+
+    const socials = [
+        {
+            name: "Github",
+            link: "https://github.com/repos",
+        },
+        {
+            name: "Mail",
+            link: "bjorn.jongenelen11@gmail.com",
+        },
+        {
+            name: "LinkedIn",
+            link: "https://www.linkedin.com/in/bjorn-jongenelen-02a4961b2/",
+        }
+    ]
+
+
     return (
         <div>
             <h1 className="mb-4 text-4xl font-bold tracking-tight text-heading md:text-5xl lg:text-6xl">
@@ -10,7 +27,21 @@ export default function Header() {
                 A enthousastic front end web developer from Belgium, Antwer with a passion for coding
             </p>
             <br />
-            <p>Driven to build user-friendly and efficient web applications that truly add value. I enjoy turning ideas into smart, working solutions.</p>
+            <p className="max-w-xl mx-auto" >Driven to build user-friendly and efficient web applications that truly add value. I enjoy turning ideas into smart, working solutions.</p>
+
+            <div className=" flex justify-center items-center gap-6 pt-10">
+                {
+                    socials.map(social => (
+                        <a target="_blank" rel="noopener noreferrer" href={`${social.link}`}>
+                            <img
+                                className="w-10 h-10"
+                                key={social.name}
+                                src={`./src/assets/icons/${social.name}.svg`}
+                                alt={`${social.name} icon`} />
+                        </a>
+                    ))
+                }
+            </div>
         </div>
     );
 }
